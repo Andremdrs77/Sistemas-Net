e6e2affb0e71a2d8d334a624288e3b57d826d7d9
André Medeiros Silva <andre1998vv@gmail.com>
2024-08-22 17:43:31 -0300

    Commit

--- Flask - 6_Databases/env/Scripts/activate.bat
+++ Flask - 6_Databases/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\20221101110015\Documents\Info1v\Flask - 6_Databases\env
+set VIRTUAL_ENV=C:\Users\20221101110015\Documents\GitHub\Sistemas-Net\Flask - 6_Databases\env
 
 if not defined PROMPT set PROMPT=$P$G
 
