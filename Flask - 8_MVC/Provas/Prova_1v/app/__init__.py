9cc3ec5a0f901ca7bddb10aeb1a6da19ae19b935
André Medeiros Silva <andre1998vv@gmail.com>
2024-11-07 17:55:13 -0300

    Add Prova 1v

---
+++ Flask - 8_MVC/Provas/Prova_1v/app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from users import users
+from books import books
+from auth import bp
+from users.models import User
+from flask_login import LoginManager, login_required
+
+app = Flask (__name__, template_folder='templates')
+
+
+app.config['SECRET_KEY']= 'SUPERDIFICIL'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+ return User.find(id=user_id)
+
+app.register_blueprint(users.bp)
+app.register_blueprint(books.bp)
+app.register_blueprint(bp.auth_bp)
+
+
+@app.route('/')
+def index():
+    return render_template('layout.html')
+
+
---
+++ Flask - 8_MVC/Provas/Prova_1v/auth/bp.py
@@ -0,0 +1,58 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import LoginManager, login_required, logout_user, login_user
+from users.models import User 
+
+auth_bp = Blueprint('auth', __name__, url_prefix='/auth', template_folder='templates')
+
+login_manager = LoginManager()
+login_manager.init_app(auth_bp)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.find(id=user_id)
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+
+        user = User(nome, email)
+        user.find()
+
+        if user:
+            login_user(user)
+
+            return redirect(url_for('users.index'))
+        else:
+            return render_template('auth/login.html')
+    return render_template('auth/login.html')
+
+    # if request.method == 'POST':
+        
+    #     nome = request.form['nome']
+    #     email = request.form['email']
+
+    #     return redirect(url_for('users.index'))
+
+    # return render_template('auth/login.html')
+
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+
+        user = User(nome, email)
+        user.save()
+
+        return redirect(url_for('auth.login'))
+    
+    return render_template('auth/register.html')
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+   # usa o método do Flask-Login para limpar a sessão do usuário.
+   logout_user()
+   return redirect(url_for('index'))
