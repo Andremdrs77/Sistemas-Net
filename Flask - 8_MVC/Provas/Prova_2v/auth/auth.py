e14eff437c3c323486d88614abb62eb2597c2f68
André Medeiros Silva <andre1998vv@gmail.com>
2024-11-12 21:40:51 -0300

    Delete Prova_2v

--- Flask - 8_MVC/Provas/Prova_2v/auth/auth.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, Blueprint, url_for, request, flash, redirect
-
-# módulo de usuários
-bp = Blueprint('auth', __name__, url_prefix='/auth', template_folder='templates')
-
-@bp.route('/login')
-def login():
-    return render_template('login.html')
--- Flask - 8_MVC/Provas/Prova_2v/users/__init__.py
+++
@@ -1,5 +0,0 @@
-
-__all__ = [
-    'users',
-    'models'
-]
