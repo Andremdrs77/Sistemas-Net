c12c59b2f274acdfa1f9914383242ceef9a92694
André Medeiros Silva <andre1998vv@gmail.com>
2024-12-12 16:15:53 -0300

    Update Aula 4 - SQLAlchemy

--- 10_SQLAlchemy/Aula 4/database/models.py
+++ 10_SQLAlchemy/Aula 4/database/models.py
@@ -1,19 +1,28 @@
-from sqlalchemy.orm import Mapped, mappedcolumn, DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
 from sqlalchemy import String, ForeignKey
+from sqlalchemy import String, ForeignKey
+from typing import List
 
 class Base(DeclarativeBase):
     pass
 
+
 class User(Base):
     __tablename__ = 'users'
-    id:Mapped[int] = mappedcolumn(primary_key=True, autoincrement=True)
-    nome:Mapped[str] = mapped_column(String(50), unique=True)
+    id:Mapped[int] = mapped_column(primary_key=True, autoincrement=True, unique=True)
+    nome:Mapped[str] = mapped_column(String(50))
+    receitas:Mapped[List['Receita']]=relationship('Receita', backref='user')
 
     def repr(self) -> str:
         return f"(User={self.nome})"
 
+
 class Receita(Base):
-    __tablename__ = 'recites'
+    __tablename__ = 'recipes'
     id:Mapped[int] = mapped_column(primary_key=True)
     nome:Mapped[str]
     user_id:Mapped[int] = mapped_column(ForeignKey('users.id'))
+
+    def __repr__(self) -> str:
+        return f"(Cumê={self.nome})"
+
--- 10_SQLAlchemy/Aula 4/exemplo2.py
+++ 10_SQLAlchemy/Aula 4/exemplo2.py
@@ -1,13 +1,27 @@
 from database.config import start_db, destroy_db, session
 from database.models import User, Receita
+from random import randint
 
 start_db()
 
 
-user = User(nome='bill')
+user = User(nome='sheldon')
 session.add(user)
 session.commit()
 
-receita1 = Receita(nome='papa')
-session.add(receita1)
+receita1 = Receita(nome='papa', user_id=user.id)
+receita2 = Receita(nome='stronogofonoff', user_id=user.id)
+session.add_all( [receita1, receita2] )
 session.commit()
+
+# re = session.query(Receita).first()
+# print(re.nome)
+# print(re.id)
+# print(re.user_id)
+print(user.receitas)
+
+# user2 = session.query(User).get(randint(101, 101)) # Versão 1
+user2 = session.get(User, randint(101, 101)) # Versão 2
+
+print(user2)
+print(user2.receitas)
